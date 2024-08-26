3c9c9430c3cd59b650b213b004050a9f00814b57
suzangyawali <sujangyawali17@gmail.com>
2024-08-26 20:47:50 +0530

    Initial Setup

---
+++ script.js
@@ -0,0 +1,9 @@
+// navbar
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('navLinks');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+//hero
